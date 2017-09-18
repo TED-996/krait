426a3d28998ddd4616df8d04fb3232039b44af29
ted_996 <budaca.eduard@gmail.com>
2017-09-18 20:21:24 +0300

    Add RaiiAlarm to simplify timing code.

---
+++ src/raiiAlarm.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <stack>
+#include <signal.h>
+
+class RaiiAlarm
+{
+	static RaiiAlarm* instance;
+	static void handler(int signal);
+	static struct sigaction* getSigaction(bool setSigInterrupt);
+
+	bool finished;
+	struct sigaction oldAction;
+public:
+	RaiiAlarm(int timeoutSeconds, bool setSigInterrupt);
+	~RaiiAlarm();
+
+	bool isFinished() const {
+		return finished;
+	}
+};
