78d6535f4cc03f70c084c6dc57c5ae22d509c660
ted_996 <budaca.eduard@gmail.com>
2017-11-06 21:39:36 +0200

    Add auto-format.py script

---
+++ auto-format.py
@@ -0,0 +1,16 @@
+import subprocess
+import os
+import glob
+
+
+def main():
+    directory = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                             "src")
+    files = glob.glob(directory + os.path.sep + "*.cpp") + glob.glob(directory + os.path.sep + "*.h")
+    if not files:
+        raise RuntimeError("No .cpp/.h files found in src.")
+    subprocess.check_call(["clang-format.exe", "-i"] + files, shell=True)
+
+
+if __name__ == '__main__':
+    main()
